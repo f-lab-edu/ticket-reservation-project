44b8262c0fbbba90ec4724e57cf01ed671a067ff
Kimdongkyun <dgk0911@gmail.com>
2025-07-20 14:05:42 +0900

    [feat/core/domain] 도메인 서비스 및 Repository 추가

---
+++ core/src/main/kotlin/com/project/core/domain/performance/service/UserPerformanceDomainService.kt
@@ -0,0 +1,16 @@
+package com.project.core.domain.performance.service
+
+import com.project.core.domain.performance.repository.PerformanceRepository
+import org.springframework.stereotype.Service
+
+
+class UserPerformanceDomainService(
+    private val performanceRepository: PerformanceRepository
+) {
+
+    fun listPerformances() {
+
+    }
+
+
+}
